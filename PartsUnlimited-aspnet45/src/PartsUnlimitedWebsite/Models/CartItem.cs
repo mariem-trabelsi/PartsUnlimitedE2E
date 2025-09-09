4ad213c3e0bc99f6ec7e94b65227d785d6d4974a
mariem-trabelsi <mrtrabelsi547@gmail.com>
2025-09-09 19:24:02 +0100

    my commit.

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace PartsUnlimited.Models
 {
+    // CartItem class represents an item in the shopping cart
     public class CartItem : ILineItem
     {
         [Key]
